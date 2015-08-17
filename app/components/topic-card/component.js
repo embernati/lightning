7b3d9221770d6e008e1cbc391c1c76cfe900e894
jimmay5469 <jimmay5469@gmail.com>
2015-08-17 01:48:01 -0400

    Add "NEW!" to topics which were created since the last meetup

--- app/components/topic-card/component.js
+++ app/components/topic-card/component.js
@@ -4,7 +4,13 @@ export default Ember.Component.extend({
   topic: null,
 
   session: Ember.inject.service(),
+  meetupDates: Ember.inject.service(),
 
+  isNew: Ember.computed('topic.submittedDate', 'meetupDates.previousMeetup', function() {
+    var submittedDate = moment(this.get('topic.submittedDate'));
+    var endOfPreviousMeetup = moment(this.get('meetupDates.previousMeetup')).endOf('day');
+    return submittedDate.isAfter(endOfPreviousMeetup);
+  }),
   isUpvoter: Ember.computed('session.currentUser', 'topic.upvoters.[]', function isUpvoter() {
     return this.get('topic.upvoters').contains(this.get('session.currentUser'));
   }),
