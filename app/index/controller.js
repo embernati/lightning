577b79b8492cdd589de486d610df882b7a446ac7
jimmay5469 <jimmay5469@gmail.com>
2015-08-16 21:21:53 -0400

    Add ability to archive topics that have been presented

--- app/index/controller.js
+++ app/index/controller.js
@@ -3,8 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
   newTopic: '',
-  topicSort: ['upvoteCount:desc', 'volunteerCount:desc'],
-  orderedTopics: Ember.computed.sort('model', 'topicSort'),
+  topicSort: ['talkDate:desc', 'upvoteCount:desc'],
+  activeTopics: Ember.computed('model.@each.talkDate', function() {
+    var now = new Date();
+    var todayBeginning = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
+    return this.get('model').filter(function(topic) {
+      var talkDate = topic.get('talkDate');
+      var talkDateBeginning = talkDate ? new Date(talkDate.getUTCFullYear(), talkDate.getUTCMonth(), talkDate.getUTCDate()) : null;
+      return !talkDate || talkDateBeginning >= todayBeginning;
+    });
+  }),
+  orderedTopics: Ember.computed.sort('activeTopics', 'topicSort'),
   actions: {
     newTopicUpdated(val) {
       this.set('newTopic', val);
