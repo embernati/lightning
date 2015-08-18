4a582e32f1d77870d92f4071a9156581ae95490e
Andy Orahoske <andrew.orahoske@zipscene.com>
2015-08-17 20:22:45 -0400

    date-format helper

---
+++ app/helpers/date-format.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function dateFormat(value, options) {
+  if (!options.format) throw Error('moment-format is expecting format to be defined.');
+  if (value.length !== 1) throw Error('moment-format is expecting one value.');
+
+  let momentValue = moment(value[0]);
+
+  if (!momentValue.isValid()) {
+    throw Error('moment-format is expecting valid Date.');
+  }
+
+  return momentValue.format(options.format);
+};
+
+export default Ember.Helper.helper(dateFormat);
---
+++ tests/unit/helpers/date-format-test.js
@@ -0,0 +1,10 @@
+import { momentFormat } from '../../../helpers/moment-format';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | moment format');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = momentFormat(42);
+  assert.ok(result);
+});
