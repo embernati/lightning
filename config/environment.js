28c9a27ef723c6216993fa8785c817e47c2c0e8f
Andy Orahoske <andrew.orahoske@zipscene.com>
2015-08-22 18:55:12 -0400

    missing semi-colon

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.firebase = 'https://embernati-lightning.firebaseio.com/'
+    ENV.firebase = 'https://embernati-lightning.firebaseio.com/';
   }
 
   return ENV;
